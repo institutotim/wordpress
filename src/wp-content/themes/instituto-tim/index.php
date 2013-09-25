eb3ac96de41db92903e9e454be9a5314877ac812
Anderson Orui <andersonorui@hacklab.com.br>
2013-09-25 16:29:02 -0300

    ajustes na single do tema

--- src/wp-content/themes/instituto-tim/index.php
+++ src/wp-content/themes/instituto-tim/index.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
 
-<h1>INDEX.php</h1>
+<?php if ( have_posts()) : while ( have_posts()) : the_post(); ?>
+
+    <?php html::part('loop'); ?>
+
+<?php endwhile; ?>
+<?php else : ?>
+    <div <?php post_class( 'col-lg-10', 'col-lg-offset-1' );?>>
+        <h1 class="entry-title">Erro 404</h1>
+        <div class="entry-content">
+            <p>A página solicitada não existe ou está fora do ar temporariamente</p>
+        </div>
+    </div>
+
+<?php endif; ?>
+
 
 <?php get_footer(); ?>
--- src/wp-content/themes/instituto-tim/parts/loop-single.php
+++ src/wp-content/themes/instituto-tim/parts/loop-single.php
@@ -1,14 +1,16 @@
 <div class="row">
-	<article id="post-<?php the_ID(); ?>" <?php post_class( array('post-entry', 'col-lg-10', 'col-lg-offset-1') );?>>
-	    <h1 class="entry-title"><?php the_title(); ?></h1>
-	    <div class="entry-content">
-	        <?php the_content(); ?>
-	    </div>
-	</article>
+    <article id="post-<?php the_ID(); ?>" <?php post_class( array('post-entry', 'col-lg-10', 'col-lg-offset-1') );?>>
+        <div class="page-header top">
+            <h1 class="entry-title top"><?php the_title(); ?></h1>
+        </div>
+        <div class="entry-content">
+            <?php the_content(); ?>
+        </div>
+    </article>
 </div>
 
 <!--/////////////////////////////////////////////////////// Comments //////////////////////////////////////////////////////-->
 
 <div class="row">
-	<?php comments_template(); ?>
+    <?php comments_template(); ?>
 </div>
