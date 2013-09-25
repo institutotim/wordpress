5982ca3d1163aac55017aa94a2667038e3ca4ce8
Anderson Orui <andersonorui@hacklab.com.br>
2013-09-25 16:19:50 -0300

    estilos para textos dentro dos posts

--- src/wp-content/themes/instituto-tim/parts/loop-single.php
+++ src/wp-content/themes/instituto-tim/parts/loop-single.php
@@ -1,12 +1,14 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class( array( 'col-lg-10', 'col-lg-offset-1') );?>>
-    <h1 class="entry-title"><?php the_title(); ?></h1>
-    <div class="entry-content">
-
-        <?php the_content(); ?>
-
-    </div>
-</article>
+<div class="row">
+	<article id="post-<?php the_ID(); ?>" <?php post_class( array('post-entry', 'col-lg-10', 'col-lg-offset-1') );?>>
+	    <h1 class="entry-title"><?php the_title(); ?></h1>
+	    <div class="entry-content">
+	        <?php the_content(); ?>
+	    </div>
+	</article>
+</div>
 
 <!--/////////////////////////////////////////////////////// Comments //////////////////////////////////////////////////////-->
 
-    <?php comments_template(); ?>
+<div class="row">
+	<?php comments_template(); ?>
+</div>
