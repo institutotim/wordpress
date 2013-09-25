fe2700d2b186370754527e2a8ae3949d80bf404c
Gutierri Barboza <gutierri@hacklab.com.br>
2013-09-25 11:37:32 -0300

    add favicon e alteracoes no title

--- src/wp-content/themes/instituto-tim/header.php
+++ src/wp-content/themes/instituto-tim/header.php
@@ -19,7 +19,7 @@
             /* Print the <title> tag based on what is being viewed. */
             global $page, $paged;
 
-            wp_title( '|', true, 'right' );
+            wp_title( '-', true, 'right' );
 
             // Add the blog name.
             bloginfo( 'name' );
@@ -27,11 +27,11 @@
             // Add the blog description for the home/front page.
             $site_description = get_bloginfo( 'description', 'display' );
             if ( $site_description && ( is_home() || is_front_page() ) )
-                echo " | $site_description";
+                echo " - $site_description";
 
             // Add a page number if necessary:
             if ( $paged >= 2 || $page >= 2 )
-                echo ' | ' . sprintf( __( 'Page %s', 'institutotim' ), max( $paged, $page ) );
+                echo ' - ' . sprintf( __( 'Page %s', 'institutotim' ), max( $paged, $page ) );
         ?></title>
         <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/img/favicon.ico" type="image/x-icon" />
         <link rel="profile" href="http://gmpg.org/xfn/11" />
