97c6b6674b627168c6669c9e29dd5267aa33bf96
Gutierri Barboza <gutierri@hacklab.com.br>
2013-09-25 16:34:59 -0300

    url de reset style no IE

--- src/wp-content/themes/instituto-tim/header.php
+++ src/wp-content/themes/instituto-tim/header.php
@@ -38,7 +38,7 @@
         <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <link rel="stylesheet/less" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/less/main.less">
-        <!--[if lt IE 8]><link rel="stylesheet" href="<?php get_stylesheet_directory_uri(); ?>/css/ie.css" type="text/css" media="screen,projection"><![endif]-->
+        <!--[if lt IE 8]><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/ie.css" type="text/css" media="screen,projection"><![endif]-->
         <!--[if lt IE 9]>
         <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
         <![endif]-->
