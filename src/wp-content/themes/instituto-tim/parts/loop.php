8c9f7142a1d1bea5d6227c3ae74888cf04844fe7
Gutierri Barboza <gutierri@hacklab.com.br>
2013-10-01 14:27:09 -0300

    Descricao dos posts abre na vertica

--- src/wp-content/themes/instituto-tim/parts/loop.php
+++ src/wp-content/themes/instituto-tim/parts/loop.php
@@ -21,10 +21,14 @@
             <div class="global-open-description-post-here">
 
             <div class="open-description-post-here icon-plus"></div>
-            
-                <ul class="description-post-here clearfix">
-                    <li class="list-category">
-                        <b>Categorias:</b><br />
+
+                <div class="row description-post-here">
+
+                    <div class="col-lg-4 col-md-4"><b>Autor:</b> <?php the_author();?></div>
+                    <div class="col-lg-4 col-md-4"><b>Em:</b> <?php the_date( 'd/m/Y' );?></div>
+                    <div class="col-lg-4 col-md-4">
+                        <div class="list-categories">
+                        <b>Categorias:</b>
                         <?php
                             //by wordpress codex
                             $categories = get_the_category();
@@ -33,7 +37,7 @@
                             if($categories) :
                                 foreach($categories as $category) :
 
-                                    $output .= '<span><a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a></span>';
+                                    $output .= '<a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a>';
 
                                 endforeach;
 
@@ -41,10 +45,9 @@
                             endif;
 
                         ?>
-                    </li>
-                    <li><b>Autor:</b> <?php the_author();?></li>
-                    <li><b>Em:</b> <?php the_date( 'd/m/Y' );?></li>
-                </ul>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 </article>
