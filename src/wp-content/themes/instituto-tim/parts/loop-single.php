336bf0e45b1a9b2b74e054040551c162978c8d49
Gutierri Barboza <gutierri@hacklab.com.br>
2013-10-02 16:30:49 -0300

    Tags na single

--- src/wp-content/themes/instituto-tim/parts/loop-single.php
+++ src/wp-content/themes/instituto-tim/parts/loop-single.php
@@ -2,7 +2,7 @@
     <article id="post-<?php the_ID(); ?>" <?php post_class( array('post-entry', 'col-lg-10', 'col-lg-offset-1', 'col-md-10', 'col-sm-12') );?>>
         <div class="page-header top">
             <h1 class="entry-title top"><?php the_title(); ?></h1>
-            <p><strong>Por:</strong> <?php the_author();?> <strong> em </strong> <?php the_date( 'd/m/Y' )?>  | <strong>Categorias: </strong> <?php the_category( ', ' );?> </p>
+            <p><strong>Por:</strong> <?php the_author();?> <strong> em </strong> <?php the_date( 'd/m/Y' )?> <strong>Categorias: </strong> <?php the_category( ', ' );?> </p>
         </div>
         <div class="entry-content">
             <?php the_content(); ?>
