d890d1e12238cc37631099d98d1ee9de3b20b39c
Gutierri Barboza <gutierri@hacklab.com.br>
2013-10-31 15:10:16 -0200

    retirada do titulo arquivo quando estivar na page solucoes

--- src/wp-content/themes/instituto-tim/archive.php
+++ src/wp-content/themes/instituto-tim/archive.php
@@ -9,6 +9,8 @@
                     printf( __( 'Arquivo mensal: %s', 'institutotim' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'institutotim' ) ) );
                 elseif ( is_year() ) :
                     printf( __( 'Arquivo anual: %s', 'institutotim' ), get_the_date( _x( 'Y', 'yearly archives date format', 'institutotim' ) ) );
+                elseif ( is_post_type_archive( 'solucoes' ) ):
+                    echo '';
                 else :
                     _e( 'Arquivo', 'institutotim' );
                 endif;
