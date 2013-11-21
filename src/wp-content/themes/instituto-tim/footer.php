7aca290c0b73a9a3b735117882d2dca07e142dfb
Anderson Orui <andersonorui@hacklab.com.br>
2013-11-21 10:13:40 -0200

    aumentando a fonte do footer

--- src/wp-content/themes/instituto-tim/footer.php
+++ src/wp-content/themes/instituto-tim/footer.php
@@ -10,11 +10,7 @@
                 <ul class="textcenter">
                     <?php $BannersApoiadores->print_items_by_position('apoiadores'); ?>
                 </ul>
-                <p class="textcenter">
-                    <small>
-                    Para entrar em contado com a equipe do Instituto TIM escreva para <a href="mailto:contato@institutotim.com.br">contato@institutotim.com.br</a>
-                    </small>
-                </p>
+                <p class="contato textcenter">Para entrar em contato com a equipe do Instituto TIM escreva para <a href="mailto:contato@institutotim.com.br">contato@institutotim.com.br</a></p>
                 <p class="textcenter"><small><?php echo get_option('text_copyright_footer');?></small></p>
             </footer>
         </div>
