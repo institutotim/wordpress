5feb1bd501545a99e10c9be06af256a5f24c7827
Gutierri Barboza <eugutierri@gmail.com>
2014-10-05 22:07:00 -0300

    textarea dos comentarios na parte superior da listagem

--- src/wp-content/themes/instituto-tim/comments.php
+++ src/wp-content/themes/instituto-tim/comments.php
@@ -26,29 +26,16 @@ add_filter('comment_class','comment_add_microid');
     <div <?php post_class( array( 'col-lg-10', 'col-lg-offset-1', 'col-md-10', 'col-md-offset-1') );?>>
         <h3 class="entry-title title-post-line"><?php _e( 'Comentários', 'institutotim' )?> (<?php comments_number('0','1', __('%','institutotim') );?>)</h3>
 
-        <?php wp_list_comments( array( 'callback' => 'comments_tim_clear' ) ); ?>
-
-        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-
-            <div class="row">
-                <div class="col-lg-10 col-md-10">
-                    <nav class="comments-nav">
-                        <div class="alignleft"><?php previous_comments_link( __( '&laquo; Older Comments', 'institutotim' ) ); ?></div>
-                        <div class="alignright"><?php next_comments_link( __( 'Newer Comments &raquo;', 'institutotim' ) ); ?></div>
-                    </nav>
-                </div>
-            </div>
-
-        <?php endif; ?>
-
         <?php if(get_option('comment_registration') && !$user_ID) : ?>
 
             <p><?php printf( __( 'You must be %sloggedin%s to post a comment.', 'institutotim'), "<a href='" . get_option('siteurl') . "/wp-login.php?redirect_to=" . urlencode(get_permalink()) ."'>", "</a>" ); ?></p>
 
         <?php else : ?>
+
             <div id="respond">
                 <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="form-comentario" class="clearfix">
                     <?php comment_id_fields(); ?>
+
                     <?php if($user_ID) : ?>
 
                         <p><?php _e('Logged in as', 'institutotim'); ?> <a href="<?php print get_option('siteurl'); ?>/wp-admin/profile.php"><?php print $user_identity; ?></a>. <a href="<?php print get_option('siteurl'); ?>/wp-login.php?action=logout" title="Logout">Logout &raquo;</a></p>
@@ -87,7 +74,22 @@ add_filter('comment_class','comment_add_microid');
 
                 </form>
             </div>
+
         <?php endif; ?>
+
+        <?php wp_list_comments( array( 'callback' => 'comments_tim_clear' ) ); ?>
+
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+            <div class="row">
+                <div class="col-lg-10 col-md-10">
+                    <nav class="comments-nav">
+                        <div class="alignleft"><?php previous_comments_link( __( '&laquo; Older Comments', 'institutotim' ) ); ?></div>
+                        <div class="alignright"><?php next_comments_link( __( 'Newer Comments &raquo;', 'institutotim' ) ); ?></div>
+                    </nav>
+                </div>
+            </div>
+        <?php endif; ?>
+
 
     </div>
 
