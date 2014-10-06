dc9301b83e8cd1052c8ce3261b5c15c95f48d59f
Gutierri Barboza <eugutierri@gmail.com>
2014-10-05 23:29:08 -0300

    ajuste na indentacao

--- src/wp-content/themes/instituto-tim/front-page.php
+++ src/wp-content/themes/instituto-tim/front-page.php
@@ -6,35 +6,32 @@
             <span id="high-light-navigation"></span>
             <div class="highlight-container">
                 <?php $destaques = new WP_Query( array('posts_per_page' => -1, 'post_type' => 'post', 'meta_key' => '_home', 'ignore_sticky_posts' => true )); ?>
-                <?php if ($destaques->have_posts()) : while ($destaques->have_posts()): $destaques->the_post(); ?>
-                <?php if ( get_the_post_thumbnail() ) :?>
-                <article id="post-<?php the_ID();?>">
-                    <div class="content">
-                        <h1><?php echo get_the_title(); ?></h1>
-                        <p class="excerpt"><?php echo wp_trim_words(get_the_excerpt(), 19, ' [...]')?></p>
-                    </div>
-                    <div class="img-wrapper">
-                        <a href="<?php echo get_permalink();?>">
-                            <?php the_post_thumbnail('destaque_high_light', array('class' => 'img')); ?>
-                        </a>
-                    </div>
-                </article>
-                
-                <?php 
-                        endif;
-                        endwhile;
-                        endif;
-                ?>
-                
+                <?php if ($destaques->have_posts()) : ?>
+                    <?php while ($destaques->have_posts()): $destaques->the_post(); ?>
+                        <?php if ( get_the_post_thumbnail() ) :?>
+                        <article id="post-<?php the_ID();?>">
+                            <div class="content">
+                                <h1><?php echo get_the_title(); ?></h1>
+                                <p class="excerpt"><?php echo wp_trim_words(get_the_excerpt(), 19, ' [...]')?></p>
+                            </div>
+                            <div class="img-wrapper">
+                                <a href="<?php echo get_permalink();?>">
+                                    <?php the_post_thumbnail('destaque_high_light', array('class' => 'img')); ?>
+                                </a>
+                            </div>
+                        </article>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
+                <?php endif;?>
             </div>
         </div>
     </div>
     <div id="about" class="col-lg-4 col-md-4 shadow">
         <article>
-            <h1><?php echo get_option('title_highlight_box');?></h1>  
+            <h1><?php echo get_option('title_highlight_box');?></h1>
             <p class="excerpt"><?php echo get_option('text_highlight_box');?></p>
             <?php $pageURLID = get_option('rela_highlight_box');
-                $linkURL = get_page_link($pageURLID); 
+                $linkURL = get_page_link($pageURLID);
             ?>
             <p class="more textright"><a href="<?php echo $linkURL;?>" class="btn btn-warning"><?php _e('Saiba mais', 'institutotim');?></a></p>
         </article>
@@ -50,7 +47,7 @@
                     </div>
                 </div>
             </div>
-            
+
             <nav id="secondary-nav" class="row">
                 <ul class="clearfix">
                     <?php $terms = get_terms('projects_categories', array('hide_empty' => 0)); ?>
@@ -71,13 +68,13 @@
                     'post_type' => 'project',
                     'posts_per_page' => -1
                 )) ?>
-                
+
                 <?php $cc = 0; ?>
 
                 <?php while( $projects->have_posts() ) : $projects->the_post(); ?>
 
                     <?php $project_categories = get_the_terms( $post->ID, 'projects_categories'); ?>
-                    
+
                     <article id="project-<?php the_ID() ?>" class="col-lg-4 col-md-4 col-sm-6">
                         <div class="img-wrapper shadow">
                             <span class="<?php foreach ($project_categories as $category) { echo $category->slug; } ?>">
