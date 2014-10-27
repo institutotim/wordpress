ff1c50cd047c29e2bfffa0853e17064147779c97
Gutierri Barboza <eugutierri@gmail.com>
2014-10-27 15:11:18 -0200

    inicializa o metabox com valor off

--- src/wp-content/themes/instituto-tim/includes/db-updates.php
+++ src/wp-content/themes/instituto-tim/includes/db-updates.php
@@ -1,7 +1,10 @@
 <?php
-/*
+
 if (congelado_db_update('db-update-1')) {
     // execute algum código aqui
+    $projetos = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type = 'project'");
+    foreach ($projetos as $pr)
+        update_post_meta($pr, 'projeto_concluido', 'off');
 }
- */
+
 ?>
