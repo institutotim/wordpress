8932f8f622b3d8ea9c0b47b50bb8e19f04faf8bc
root <root@debian>
2016-10-14 19:59:01 +0000

    update

--- src/wp-content/themes/instituto-tim/footer.php
+++ src/wp-content/themes/instituto-tim/footer.php
@@ -17,10 +17,43 @@
                 </ul>
                 <!-- <p class="contato textcenter">Para entrar em contato com a equipe do Instituto TIM escreva para <a href="mailto:contato@institutotim.com.br">contato@institutotim.com.br</a></p> -->
                 <p class="textcenter"><small><?php echo get_option('text_copyright_footer');?></small></p>
+
             </footer>
         </div>
     </div>
-    
+
 <?php wp_footer(); ?>
+
+<?php /*
+Google Analytics 
+https://developers.google.com/analytics/devguides/collection/analyticsjs/cookies-user-id#using_localstorage_to_store_the_client_id
+*/
+?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+var GA_LOCAL_STORAGE_KEY = 'ga:clientId';
+
+if (window.localStorage) {
+  ga('create', 'UA-49342801-1', {
+    'storage': 'none',
+    'clientId': localStorage.getItem(GA_LOCAL_STORAGE_KEY)
+  });
+  ga(function(tracker) {
+    localStorage.setItem(GA_LOCAL_STORAGE_KEY, tracker.get('clientId'));
+  });
+}
+else {
+  ga('create', 'UA-49342801-1', 'auto');
+}
+
+ga('send', 'pageview');
+
+</script>
+
 </body>
 </html>
